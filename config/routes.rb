e9ee3bb3c5da1527b8b208f537cdff38c71c4d99
pandaulait <show.hey.panda@gmail.com>
2021-10-15 14:04:34 +0000

    [Update]User,Admin controller view routing 作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions:      'public/devise/sessions',
+    registrations: 'public/devise/registrations'
+  }
+  devise_for :admins,
+  path: :admin,
+  controllers: {
+    sessions: 'admin/devise/sessions',
+    registrations: 'admin/devise/registrations'
+  }
+  
+  scope module: :public do
+    root to: 'homes#top'  
+  end
 end
