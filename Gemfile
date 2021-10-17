25183a9768c51a7969ecdf008a268245b9279473
pandaulait <show.hey.panda@gmail.com>
2021-10-17 14:27:10 +0000

    [Update]プロフィール画像

--- Gemfile
+++ Gemfile
@@ -63,3 +63,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
 gem 'devise'
+gem 'image_processing', '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  has_one_attached :profile_image
+  
+  
+  
+  
+  
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
