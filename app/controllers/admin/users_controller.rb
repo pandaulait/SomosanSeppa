011b2586dcea76f5255ae74de21c475a01c4c05b
pandaulait <show.hey.panda@gmail.com>
2021-10-22 14:43:39 +0000

    [Update]退会済み編集

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,11 +3,21 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
-
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to(admin_users_path)
+  end
+  
   private
   
   def admin_user
     redirect_to root_path unless current_user.admin?
+  end
+  
+  def user_params
+    params.require(:user).permit(:is_deleted)
   end
 
 end
