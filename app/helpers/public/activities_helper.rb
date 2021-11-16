0872f0613bc20cb0b5316eaefcb58c4cf8762884
pandaulait <show.hey.panda@gmail.com>
2021-11-16 16:39:36 +0000

    [Update]通知機能 view #56

--- app/helpers/public/activities_helper.rb
+++ app/helpers/public/activities_helper.rb
@@ -2,9 +2,9 @@ module Public::ActivitiesHelper
   def transition_path(activity)
     case activity.action_type.to_sym
     when :chatted_by_admin
-      chat_rooms_path
+      chat_rooms_path(anchor: "chat-#{activity.subject.id}")
     when :chatted_by_user
-      admin_chat_room_path(activity.subject.chat_room, anchor: "js-comment-#{activity.subject.id}")
+      admin_chat_room_path(activity.subject.chat_room, anchor: "chat-#{activity.subject.id}")
     when :leveled_up
       user_path(activity.subject)
     end
