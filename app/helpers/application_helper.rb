caac6ad41db13c1fae30296a76a8776a1525a322
pandaulait <show.hey.panda@gmail.com>
2021-11-17 04:15:17 +0000

    [Update]解説に外部リンク設置化、確認ダイアログの表示 #31

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
   def bootstrap_alert(key)
     TITLES[key]
   end
+  require 'uri'
+
+  def text_url_to_link(text)
+    URI.extract(text, %w[http https]).uniq.each do |url|
+      sub_text = ''
+      sub_text << '<a href=' << url << " target=\"_blank\" onclick=\"return confirm('外部のページへ移動します。よろしいですか？')\">" << url << '</a>'
+
+      text.gsub!(url, sub_text)
+    end
+    text
+  end
 end
