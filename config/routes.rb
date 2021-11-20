64ae0c13360152a10417d18c6242441ff5fdc2eb
pandaulait <show.hey.panda@gmail.com>
2021-11-20 09:46:54 +0000

    [Update]ユーザー側 quizzes　indexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
     end
     get 'quizzes/somosan', to: 'quizzes#somosan'
     get 'quizzes/seppa', to: 'quizzes#seppa'
+    resources :quizzes, only: %i[index]
+    
     patch 'selection_quiz_image_destroy/:id' => 'selection_quizzes#image_desttroy', as: 'selection_quiz_image_destroy'
     resources :selection_quizzes do
       resources :results, only: %i[create show]
